c156f1143743bdf372bf6f73111293cd99023724
VarunPattiah <varunpattiah@gmail.com>
2016-12-21 23:03:16 -0500

    Replicated second server send the response to leader

---
+++ ReplicaTwo/src/server/FailureDetection.java
@@ -0,0 +1,86 @@
+package server;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class FailureDetection extends Thread {
+
+	
+	@Override
+	public void run() {
+		new MulticastMessageReceiver(Constants.MC_FD_PORT1);
+		new MulticastMessageReceiver(Constants.MC_FD_PORT3);
+		while(true){
+			List<String> activeProcesses = new MulticastMessageSender().multicastRequest("FD", Constants.MC_FD_PORT2);
+			int currentGroupLeader = new MainServerProject().getGroupLeaderProcessID();
+			System.out.println("Current FD-Replica3 GroupLeader: "+currentGroupLeader);
+			System.out.println("activeProcesses.size() : "+activeProcesses.size());
+			for(String str:activeProcesses)
+				System.out.println(str);
+			
+			if (activeProcesses.size() == 0) {
+				try {
+					notifyFrontEnd(InetAddress.getLocalHost().getHostAddress());
+				} catch (UnknownHostException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}else{
+				List<Integer> processes =  new ArrayList<Integer>();;
+				for(String process: activeProcesses){
+					processes.add(Integer.parseInt(process.split(":")[0]));
+				}
+				
+				if(!processes.contains(currentGroupLeader)){
+					System.out.println("Leader Election System started...");
+					new LeaderElection(activeProcesses).start();
+				}
+				else if(currentGroupLeader == Constants.PROCESS_ID){
+					new MulticastMessageSender().multicastRequest("LE:"+Constants.PROCESS_ID, Constants.MC_FD_PORT2);
+					try {
+						notifyFrontEnd("LCHANGED:"+InetAddress.getLocalHost().getHostAddress());
+					} catch (UnknownHostException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			}
+			try {
+				Thread.currentThread().sleep(5000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}	
+	}
+
+	/**
+	 * method which notifies the front end about new group leader
+	 * @param address
+	 */
+	private void notifyFrontEnd(String address) {
+		DatagramSocket socket = null;
+		DatagramPacket packet = null;
+		try{
+			socket = new DatagramSocket();
+			byte[] bytes = address.getBytes();
+			packet = new DatagramPacket(bytes, bytes.length, InetAddress.getByName(new MainServerProject().getFrontEndAddress()), Constants.FE_NOTIFY_PORT);
+			socket.send(packet);
+		}catch(Exception e){
+			System.out.println("IO:"+e.getMessage());
+		}finally{
+			if(null != socket)
+				socket.close();
+		}
+		
+	}
+	
+	
+
+}
---
+++ ReplicaTwo/src/server/UDPClient.java
@@ -0,0 +1,84 @@
+package server;
+
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.ObjectInputStream;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.ArrayList;
+
+
+
+public class UDPClient extends Thread {
+
+	private String serverAddress;
+	private int udpPort;
+	private byte[] data;
+	PassengerWrapperVO resultVO;
+	ArrayList<PassengerWrapperVO> serverResponse = new ArrayList<PassengerWrapperVO>();
+	Constants.REQUEST_TYPE requestType;
+
+	public String getServerAddress() {
+		return serverAddress;
+	}
+
+	public void setServerAddress(String serverAddress) {
+		this.serverAddress = serverAddress;
+	}
+
+	public int getUdpPort() {
+		return udpPort;
+	}
+
+	public void setUdpPort(int udpPort) {
+		this.udpPort = udpPort;
+	}
+
+	public byte[] getData() {
+		return data;
+	}
+
+	public void setSendData(byte[] data) {
+		this.data = data;
+	}
+
+	public UDPClient(String serverAddress, int udpPort, byte[] data, Constants.REQUEST_TYPE requestType,ArrayList<PassengerWrapperVO> serverResponse) {
+		this.setServerAddress(serverAddress);
+		this.setUdpPort(udpPort);
+		this.setSendData(data);
+		this.requestType = requestType;
+		this.serverResponse = serverResponse;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Thread#run()
+	 */
+	@Override
+	public void run() {
+		DatagramSocket datagramSocket = null;
+		try {
+			datagramSocket = new DatagramSocket();
+			InetAddress hostAddress = InetAddress.getByName(this.getServerAddress());
+			int serverPort = this.getUdpPort();
+			DatagramPacket request = new DatagramPacket(this.getData(), this.getData().length, hostAddress,
+					serverPort);
+			datagramSocket.send(request);
+			byte[] message = new byte[5000];
+			DatagramPacket reply = new DatagramPacket(message, message.length);
+			datagramSocket.receive(reply);
+
+			ByteArrayInputStream bais = new ByteArrayInputStream(message);
+			ObjectInputStream is = new ObjectInputStream(new BufferedInputStream(bais));
+	
+			resultVO = (PassengerWrapperVO) is.readObject();
+			serverResponse.add(resultVO);
+			is.close();
+			bais.close();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		} finally {
+			datagramSocket.close();
+		}
+	}
+}
